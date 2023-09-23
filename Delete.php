3f0ae94be9016a7dd6dbdbacbcbe70426be00b38
Sijan Shrestha <84898822+shrsijan@users.noreply.github.com>
2023-09-23 10:55:06 -0400

    Update Delete.php

--- Delete.php
+++ Delete.php
@@ -1,13 +1,26 @@
 <?php
 include 'db.php';
-if(isset($_GET['deleteid'])){
- $id=$_GET['deleteid'];
- $sql="delete from `details` where id=$id";
- $result=mysqli_query($con,$sql);
- if($result){
- echo "Deleted Successfully";
- header("location:display.php");
- }else{
- die(mysqli_error($con));
- }
+
+if (isset($_GET['deleteid'])) {
+    $id = $_GET['deleteid'];
+
+    // Validate the ID (you should also check if it's an integer)
+    if (!is_numeric($id)) {
+        die("Invalid ID");
+    }
+
+    $sql = "DELETE FROM `details` WHERE id=?";
+    $stmt = mysqli_prepare($con, $sql);
+    mysqli_stmt_bind_param($stmt, "i", $id);
+
+    if (mysqli_stmt_execute($stmt)) {
+        // Deleted successfully
+        echo "Deleted Successfully";
+        header("location: display.php");
+        exit(); // Terminate script to prevent further execution
+    } else {
+        // Handle database error
+        die("Error: " . mysqli_error($con));
+    }
 }
+?>
