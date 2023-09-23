5e951f55bb063fe28c67bc4b933a792a0f44b575
Sijan Shrestha <84898822+shrsijan@users.noreply.github.com>
2023-09-23 10:56:29 -0400

    Update Database.php

--- Database.php
+++ Database.php
@@ -1,5 +1,20 @@
 <?php
-$con= new mysqli('localhost','root','','siz');
-if(!$con){
- die(mysqli_error($con));
+// Define database connection constants
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'siz');
+
+// Create a database connection
+$con = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+
+// Check if the connection was successful
+if ($con->connect_error) {
+    die("Connection failed: " . $con->connect_error);
 }
+
+// ... Perform database operations ...
+
+// Close the database connection when done
+$con->close();
+?>
